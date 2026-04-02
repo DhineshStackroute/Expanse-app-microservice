d4e90fcae7858ac80118eadf72b63a820e35ca75
Dhinesh.Kumar <dhinesh.kumar@stackroute.in>
2026-04-02 17:15:51 +0530

    swagger integration

--- Expanse_Servoioce/app.js
+++ Expanse_Servoioce/app.js
@@ -2,12 +2,14 @@
 const express=  require('express');
 // include app services
 const appService= require('./app.service');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 // Create  a Exapanse app usig express
 const app = express();
 
 // export the app to other files
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 appService.connectToDatabase();
 appService.setMiddleWare(app);
 appService.apiSetUp(app);
